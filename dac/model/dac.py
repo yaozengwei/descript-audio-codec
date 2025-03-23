e62005948a3b0a6cc81d06c26ffa21f10b6b6bce
yaozengwei <yaozengwei@outlook.com>
2025-03-23 16:44:40 +0800

    fix bug

--- dac/model/dac.py
+++ dac/model/dac.py
@@ -102,6 +102,7 @@ class DecoderBlock(nn.Module):
                 kernel_size=2 * stride,
                 stride=stride,
                 padding=math.ceil(stride / 2),
+                output_padding=0 if stride % 2 == 0 else 1,
             ),
             ResidualUnit(output_dim, dilation=1),
             ResidualUnit(output_dim, dilation=3),
