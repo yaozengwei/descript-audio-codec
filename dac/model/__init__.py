9ca1292710d96c1f26279e2a2ba3325f5e4d38d6
yaozengwei <yaozengwei@outlook.com>
2025-03-31 17:24:40 +0800

    add dac_add_noise

--- dac/model/__init__.py
+++ dac/model/__init__.py
@@ -3,5 +3,6 @@ from .base import DACFile
 from .dac import DAC
 from .discriminator import Discriminator
 from .dac_no_vq import DACNoVq
+from .dac_add_noise import DACAddNoise
 from .convnext import FlowMatching
 from .hidden_generator import HiddenGenerator
